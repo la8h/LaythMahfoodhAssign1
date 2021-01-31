eef26efeeb6cdfdfed08b1388b4c538d1dd3d783
mahfoodh1 <mahfoodh@sheridancollege.ca>
2021-01-30 22:05:06 -0500

    Initial commit

---
+++ LaythLab11/src/main/java/layth/mahfoodh/s991523983/LaythActivity.java
@@ -0,0 +1,19 @@
+/*
+Layth Mahfoodh s991523983
+This is assignment 1 to demonstrate the use of
+activities and how to invoke 3rd party app
+ */
+package layth.mahfoodh.s991523983;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LaythActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ LaythLab11/src/main/java/layth/mahfoodh/s991523983/MahfoodhActivity.java
@@ -0,0 +1,19 @@
+/*
+Layth Mahfoodh
+This is assignment 1 to demonstrate the use of
+activities and how to invoke 3rd party app
+ */
+package layth.mahfoodh.s991523983;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MahfoodhActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mahfoodh);
+    }
+}
