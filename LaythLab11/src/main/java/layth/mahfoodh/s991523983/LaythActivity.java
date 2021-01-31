2e1e4392108e8ba29d8325e1bc99e05e05904641
mahfoodh1 <mahfoodh@sheridancollege.ca>
2021-01-31 16:17:35 -0500

    Add Background color, button, EditText, and welcoming phrase to main activity.

--- LaythLab11/src/main/java/layth/mahfoodh/s991523983/LaythActivity.java
+++ LaythLab11/src/main/java/layth/mahfoodh/s991523983/LaythActivity.java
@@ -7,7 +7,10 @@ package layth.mahfoodh.s991523983;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 public class LaythActivity extends AppCompatActivity {
 
@@ -15,5 +18,16 @@ public class LaythActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void callWeb(View view){
+        Intent intent = null;
+        switch (view.getId()) {
+            case R.id.btnBrowser:
+                intent = new Intent(Intent.ACTION_VIEW,
+                        Uri.parse("https://www.cp24.com"));
+                startActivity(intent);
+                break;
+
+        }
     }
 }
