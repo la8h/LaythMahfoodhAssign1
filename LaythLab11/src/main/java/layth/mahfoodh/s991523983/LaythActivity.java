16ee8f36a5b75829aafcd8cf505ac6030dd221f7
mahfoodh1 <mahfoodh@sheridancollege.ca>
2021-01-31 19:42:55 -0500

    Display a toast wit full name and web name.

--- LaythLab11/src/main/java/layth/mahfoodh/s991523983/LaythActivity.java
+++ LaythLab11/src/main/java/layth/mahfoodh/s991523983/LaythActivity.java
@@ -7,10 +7,13 @@ package layth.mahfoodh.s991523983;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
+
 
 public class LaythActivity extends AppCompatActivity {
 
@@ -23,11 +26,21 @@ public class LaythActivity extends AppCompatActivity {
         Intent intent = null;
         switch (view.getId()) {
             case R.id.btnBrowser:
+            Context context = getApplicationContext();
+            CharSequence text =getString(R.string.label_toast);
+            int duration = Toast.LENGTH_LONG;
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+
                 intent = new Intent(Intent.ACTION_VIEW,
                         Uri.parse("https://www.cp24.com"));
                 startActivity(intent);
                 break;
-
+            case R.id.btnActivity:
+                intent = new Intent(  this, MahfoodhActivity.class);
+                startActivity(intent);
+            default:
+                break;
         }
     }
 }
